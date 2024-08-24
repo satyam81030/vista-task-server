0496bf8c253b8396a2b1829d36ed43e30ac4d6c2
satyam@81030 <solankisatyam3105@gmail.com>
2024-08-24 08:29:48 +0530

    fix(cors): fixed access

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const userController = require("../controllers/usersControllers");
 const authMiddleware = require("../middleware/auth")
 
-router.post("/user-meta", authMiddleware.auth, authMiddleware.isUser, userController.addOrUpdateUserMeta);
+router.post("/user-meta", authMiddleware.auth, userController.addOrUpdateUserMeta);
 router.get("/users",  authMiddleware.auth, authMiddleware.isAdmin, userController.getAllUsers);
 router.post("/users",authMiddleware.auth, authMiddleware.isAdmin, userController.addUser)
 router.get("/users/:id", userController.getUserMetaByUserId); // New route to get user details by ID
