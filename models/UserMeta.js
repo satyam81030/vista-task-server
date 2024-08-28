f525093710ed374a1f4498a92085d46a07afbbc8
satyam81030 <110453521+satyam81030@users.noreply.github.com>
2024-08-28 15:10:33 +0530

    Update UserMeta.js

--- models/UserMeta.js
+++ models/UserMeta.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userMetaSchema = new Schema({
-  userId:  { type: String, required: true },, // Reference to User
+  userId: { type: Schema.Types.ObjectId, ref: "User", required: true }, // Reference to User
  
   // Personal Details
   firstName: { type: String, required: true },
