653140d34939841882b59231f6c6081efe870571
satyam81030 <110453521+satyam81030@users.noreply.github.com>
2024-09-08 23:18:26 +0530

    time

--- controllers/authController.js
+++ controllers/authController.js
@@ -27,8 +27,8 @@ exports.login = async (req, res) => {
     // Define allowed login hours
     const startHour = 9;
     const startMinute = 0;
-    const endHour = 22;
-    const endMinute = 45;
+    const endHour = 23;
+    const endMinute = 59;
 
     // Check if current time is within the allowed login hours
     if (
